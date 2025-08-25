609c1b3a37d81029d2420f7722af9f148827e1a8
niklalgupta7 <niklalgupta7@gmail.com>
2025-08-25 20:29:19 +0530

    Delete Ma044ch1delpRjK0044 directory

--- Ma044ch1delpRjK0044/js/custom.js
+++
@@ -1,9 +0,0 @@
-
-function chat() {
-document.getElementById("chat").style.display = "block";
-document.getElementById("footer").style.display = "block";
-document.getElementById("welcomeDiv").style.display = "block";
- document.getElementById("chat-box").style.display = "none";
- document.getElementById("login-box").style.display = "none";
-
-}
